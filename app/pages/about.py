962a9cf717508576629c714d7d559a587822bc98
Alexander Fastner <alexanderfastner@gmail.com>
2025-04-03 19:52:51 -0700

    fill out about me and add to Cantata

--- app/pages/about.py
+++ app/pages/about.py
@@ -13,39 +13,60 @@ about = (
 
             #TODO Add images of me
             #TODO  Write better intro text - similar as linkedin
+            #TODO add tum and LMU better links and images
+
             dbc.Card(
                 dbc.CardBody(
                     dcc.Markdown(
                         '''
                         # About  Me:
-                        3-4 sentence intro
                         
-                        ### Hobbies
-                            diving
-                            Random projects: Arduino
-                                             Android Apps
-                                             Raspberry Pi
+                        In my free time, I immerse myself in a variety of engaging hobbies that keep my mind active and satisfy my sense of adventure.
+                        One of my greatest passions is diving, where I explore the serene underwater world, discovering vibrant marine ecosystems and 
+                        experiencing the peaceful weightlessness that only exists beneath the waves. 
+                        When I'm not underwater, I pour my creative energy into various tech projects that challenge my problem-solving skills and 
+                        keep me constantly learning something new.
+
+                        One aspect of programming that has always interested me, but never came up in my studies, was embedded Programming.
+                        I've recently enjoyed learning soldering and tinkering with an Arduino board to create a custom physical volume control for my PC.
                         
-                        ### Education
+                        Another project I've wanted to work on is to make my own app. To this end I took an Android development course
+                        and began on designing a mobile version of the game machine strike. This is a board stategy game similar to chess, and I 
+                        continuosly work on it in my free time getting it closer and closer to a playable version.
+                        
+                        The versatile Raspberry Pi has become another favorite platform for my experiments. Most recently I want to set it up as a web host for this site.
+                        Figuring out the ins and outs of the OS as well as setting up networking, security and port forwarding was quite the interesting challenge. 
+                        From this I learned some valuable skills that I can continue to use for various projects in the future.
+
+                        All of these projects along with many others are linked in my portfolio.
+                        
+                        ## Education
                         I am a Bay Area native who pursued higher education in Germany, by leveraging my dual citizenship and German language proficiency. 
                         In Munich I began on my Bachelors in Bioinformatics in 2022 in a program shared between the two large universities in Munich. 
-                            Technical University of Munich - add link
-                            Ludwig-Maximilians-Universitaet - add link
+                            Technical University of Munich - https://www.tum.de/en/
+                            Ludwig-Maximilians-Universitaet - https://www.lmu.de/en/
                         
+                        ### Search for evidence of recombination in Alternaria solani using genomic tools.
+                            Bioinformatics Bachelor (TUM/LMU)
+
+                        The fungal pathogen Alternaria solani is known to cause early blight in tomatoes and potatoes and causes millions in economic losses every year. 
+                        In my thesis I use genomic tools (LDHelmet, clonalframeML) to analyze data from 48 isolates and search for evidence that the genomic variation we 
+                        see across isolates is caused by an unknown sexual reproduction phase as opposed to asexual reproduction. 
+                        In addition to the two above mentioned tools the data was cleaned, and pre-processed in bash. Then the resulting output was visualized in R.
+                        I went on to continue this work as research assistant for Prof. Stam at the Phytopathology chair at TUM and also later at Kiel University.
+
+                        ### Bioinformatic Analysis of the role of MAFA in Stem Cell-Derived Human Pancreatic Islets.
+                            Bioinformatics Master (TUM/LMU + Helmholtz Institute) 
                         
-                        
-                        Talk about various lectures...
-                        ...
-                        ...
+                        In this research collaboration with (Theis Lab Helmholtz and Hebrok Lab TUM), I conducted a bioinformatics analysis focusing on Type 1 diabetes
+                        and stem cell-derived β cell maturation. Using Bulk RNA sequencing data from the Transcription Factor MAFA and its naturally occurring mutant 
+                        MAFAS64F cell lines, I employed various bioinformatics methods to identify differentially expressed genes, as well as Transcription Factors and
+                        Pathways of interest. Despite the limitations of the available bulkRNA data, my work established a solid foundation for future investigations
+                        into MAFA's role in β cell maturation and insulin production, contributing to the broader goal of developing stable, functional stem cell-derived
+                        β cells for potential Type 1 diabetes treatment. In the future my findings will serve as a comparison point for a single-cell RNAseq analysis.
 
-                        TUM/LMU Bioinformatics Bachelor - title/topic
-
-                        other lectures in mmy masters program
-
-                        TUM/LMU Bioinformatics Master- title/topic
 
                         I completed my M.Sc. in 2024 and moved back to the United States to start my Career.
-                        
                         If you are interested in my work, you can find more information on my linkedIn.                        
                     '''),
                 ),
--- app/pages/cantata.py
+++ app/pages/cantata.py
@@ -6,24 +6,69 @@ from dash import dcc, html, callback
 #-----------------------------------------------------------
 dash.register_page(__name__, path="/Cantata")
 #-----------------------------------------------------------
-layout = (
-    html.Div(
-        style={"display": "flex", "justify-content": "center"},
-        children=[
-            html.H1("Cantata"),
-            
-            dbc.Card(
-                dbc.CardBody(
-                    dcc.Markdown('''
-                                 What is this\n
-                                 link to site\n
-                                 link to githubs\n
-                                 ...\n
-                                 '''),
-                ),
-                style={"width": "60%"},
+heatmap_example = "Cantata/heatmap_example.png"
+stacked_area_example = "Cantata/stacked_area_example.png"
+raincloud_example = "Cantata/raincloud_example.png"
+alignment_example = "Cantata/alignment_example.png"
+#-----------------------------------------------------------
+info = html.Div(
+    # style={"display": "flex"},
+    children=[
+        dbc.Card(
+            dbc.CardBody(
+                dcc.Markdown(
+                    '''
+
+                    ### CANTATA\n
+
+                    **CANTATA** is a (**C**ommunity b**A**sed **N**on-bila**T**eri**A**n **T**ranscriptome **A**rchive).\n
+
+                    The aim of this project is to provide an archive of non-bilaterian transcriptomic resources assembled and annotated in a standardized manner.
+                    I solo developed the frontend and visualization tools for this resource in python dash and hosted it using docker and heroku.
+                    It is intended to serve as a resource for Biologists to analyze and interpret their sequencing data for aquatic life.\n
+
+                    On your right you can see some examples of various visualizations possible with this site.
+
+                    Check it out here! [Canata](https://github.com/AlexanderFastner/cantata_frontend)\n
+                    If you are interested in the code check my [Github site](https://cantatadb-8f2293883fc5.herokuapp.com/)\n
+
+                '''),
             ),
-        ]
-    )
+            # style={"width": "60%"},
+        ),
+    ]
+)
+#-----------------------------------------------------------
+examples = html.Div(
+    children=[
+        dbc.Card([
+            dbc.CardBody(html.H5("Heatmap Example")),
+            dbc.CardImg(src=dash.get_asset_url(heatmap_example), className="w-100"),
+        ]),
+        dbc.Card([
+            dbc.CardBody(html.H5("Stacked Area Example")),
+            dbc.CardImg(src=dash.get_asset_url(stacked_area_example), className="w-100"),
+        ]),
+        dbc.Card([
+            dbc.CardBody(html.H5("Raincloud Example")),
+            dbc.CardImg(src=dash.get_asset_url(raincloud_example), className="w-100"),
+        ]),
+        dbc.Card([
+            dbc.CardBody(html.H5("Alignment Comparison Example")),
+            dbc.CardImg(src=dash.get_asset_url(alignment_example), className="w-100"),
+        ]),
+    ]
+)
+
+#-----------------------------------------------------------
+layout = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(info, width=5),
+                dbc.Col(examples, width=7),
+            ]
+        )
+    ]
 )
 #-----------------------------------------------------------
